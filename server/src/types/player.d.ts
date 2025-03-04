0479a05c1e309d869690fac5fdd17b0bcecee8e8
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-04 17:28:36 +0000

    feat: define a type for the player

---
+++ server/src/types/player.d.ts
@@ -0,0 +1,15 @@
+type Player = {
+  firstName: string,
+  lastName: string,
+  fullName: string,
+  birthDate: string,
+  nationality: string,
+  marketValue: number,
+  currentClub: number,
+  position: string,
+  age: number,
+  PA: number,
+  CA: number
+}
+
+export default Player;
