fd5fc81c1224fc199eb709a2474658c25a3eea83
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-04 17:28:54 +0000

    feat: define a type for the player attributes

---
+++ server/src/types/attributes.d.ts
@@ -0,0 +1,8 @@
+type Attributes = {
+  finishing: number, crossing: number, dribbling: number, heading: number, tackling: number, marking: number, passing: number, free_kick: number,
+  acceleration: number, agility: number, strength: number, jumping: number,
+  vision: number, decision: number, positioning: number, antecipation: number, aggression: number,
+  reflexes: number, handling: number, diving: number
+}
+
+export default Attributes;
