e81af0f08ab3cc4d8806f641527a9794bf37a3c5
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-04 10:11:39 +0000

    feat: create generic functions to add data to the database (seed)

--- server/src/services/seed/createInitialSeed.ts
+++ server/src/services/seed/createInitialSeed.ts
@@ -4,6 +4,26 @@ import checkSeedDone from "./checkSeedStatus";
 import insertIntoDB from "../../utils/seed/databaseInsert";
 import loadJsonDataFile from "../../utils/seed/loadJsonData";
 
+const insertStadiumsDB = async (nation: string, league: string) => {
+  const stadiums = loadJsonDataFile(path.join(__dirname, '../', '../', 'seed', 'data', 'stadiums', `${nation}`,`${league}_stadiums.json`));
+  await insertIntoDB('club', ["idClub", "name", "founded", "nickname", "reputation", "logo", "formation", "idStadium", "idCountry"], stadiums);
+}
+
+const insertClubsDB = async (nation: string, league: string) => {
+  const clubs = loadJsonDataFile(path.join(__dirname, '../', '../', 'seed', 'data', 'clubs', `${nation}`,`${league}_clubs.json`));
+  await insertIntoDB('club', ["idClub", "name", "founded", "nickname", "reputation", "logo", "formation", "idStadium", "idCountry"], clubs);
+}
+
+const insertCoachesDB = async (nation: string, league: string) => {
+  const coaches = loadJsonDataFile(path.join(__dirname, '../', '../', 'seed', 'data', 'coaches', `${nation}`,`${league}_coaches.json`));
+  await insertIntoDB("coach", ["idCoach", "name", "nationality", "currentClub", "isUser"], coaches);
+}
+
+const insertCompetitions = async (nation: string) => {
+  const englandCompetitions = loadJsonDataFile(path.join(__dirname, 'data', 'competitions', nation, `${nation}_competitions.json`));
+  await insertIntoDB("competition", ["idCompetition", "idCountry", "name", "type", "season", "logo"], englandCompetitions);
+}
+
 const seedDatabase = async () => {
   const databaseAlreadySeeded = await checkSeedDone();
 
@@ -11,6 +31,12 @@ const seedDatabase = async () => {
     if (databaseAlreadySeeded.count === 0) {
       const countries = loadJsonDataFile(path.join(__dirname, '../', '../', 'seed', 'data', 'international', 'countries_data.json'));
       await insertIntoDB('country', ["idCountry", "name", "code", "continent", "ranking", "nationalTeam"], countries);
+
+      //ENGLAND - PREMIER LEAGUE
+      insertStadiumsDB("england", "premier_league");
+      insertClubsDB("england", "premier_league");
+      insertCoachesDB("england", "premier_league");
+      insertCompetitions("england");
 
       markSeedDone();
     }
